ad45fde0a4ddfb48d506e7b72b9828eed808fa6c
SauravSrivastava27 <saurav.srivastava27@outlook.com>
2020-07-19 21:04:44 +0530

    Second Commit

---
+++ CRM/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult customer()
+        {
+            return View();
+        }
+
+        public ActionResult customerInfo()
+        {
+            return View();
+        }
+    }
+}
