6614679b72ed376195760b3acf909f91ad587d39
SauravSrivastava27 <saurav.srivastava27@outlook.com>
2020-07-26 22:49:34 +0530

    add new controller, banner, location and product

--- CRM/Script/state.js
+++ CRM/Script/state.js
@@ -1,4 +1,4 @@
-﻿$('#default-ordering').DataTable({
+﻿$('#state-table').DataTable({
     "oLanguage": {
         "oPaginate": { "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>', "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>' },
         "sInfo": "Showing page _PAGE_ of _PAGES_",
@@ -6,10 +6,10 @@
         "sSearchPlaceholder": "Search...",
         "sLengthMenu": "Results :  _MENU_"
     },
-    "order": [[3, "desc"]],
+    "order": [[1, "asc"]],
     "stripeClasses": [],
     "lengthMenu": [5, 10, 20, 50],
-    "pageLength": 10,
+    "pageLength": 5,
     "sDom": 'rlftip',
     drawCallback: function () { $('.dataTables_paginate > .pagination').addClass(' pagination-style-13 pagination-bordered mb-5'); }
 });
@@ -18,11 +18,11 @@ var addDeleteIcon = '<div class="deleteButton">';
 addDeleteIcon += '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2  delete-multiple"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>';
 addDeleteIcon += '</div>';
 
-addDeleteIcon += '<a href="/Customer/customerInfo" class="addButton">';
+addDeleteIcon += '<div data-toggle="modal" data-target="#stateModal" class="addButton">';
 addDeleteIcon += '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>';
-addDeleteIcon += '</a>';
+addDeleteIcon += '</div>';
 
-$("#default-ordering_filter").append(addDeleteIcon);
+$("#state-table_filter").append(addDeleteIcon);
 
 
 // Delete Alert
