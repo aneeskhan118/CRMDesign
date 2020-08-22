adb2a41aaba6c2e173a1fd059de249617a4d0c19
SauravSrivastava27 <saurav.srivastava27@outlook.com>
2020-08-22 23:59:27 +0530

    rest of the changes except feedback

---
+++ CRM/Controllers/NotificationController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM.Controllers
+{
+    public class NotificationController : Controller
+    {
+        // GET: Notification
+        public ActionResult notificationMarketing()
+        {
+            return View();
+        }
+
+        public ActionResult splashMarketing()
+        {
+            return View();
+        }
+
+        public ActionResult systemSettings()
+        {
+            return View();
+        }
+
+        public ActionResult accessPermission()
+        {
+            return View();
+        }
+
+        public ActionResult userGroup()
+        {
+            return View();
+        }
+
+        public ActionResult sendSMS()
+        {
+            return View();
+        }
+
+        public ActionResult sendPush()
+        {
+            return View();
+        }
+
+        public ActionResult viewUser()
+        {
+            return View();
+        }
+
+        public ActionResult addUser()
+        {
+            return View();
+        }
+    }
+}
---
+++ CRM/Controllers/OrdersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult order()
+        {
+            return View();
+        }
+
+        public ActionResult orderRejectList()
+        {
+            return View();
+        }
+
+        public ActionResult customOrder()
+        {
+            return View();
+        }
+
+        public ActionResult customOrderRejectList()
+        {
+            return View();
+        }
+
+        public ActionResult unsoldCart()
+        {
+            return View();
+        }
+
+        public ActionResult cartList()
+        {
+            return View();
+        }
+    }
+}
